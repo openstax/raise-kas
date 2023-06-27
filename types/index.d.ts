cc83e76555b190bcf542d9611f85d0d709f90bcf
Michael <mreyna12@live.com>
2023-06-27 14:40:56 -0500

    Update type definitions

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,4 @@
 declare module '@khanacademy/kas' {
-  // TODO: These are placeholder definitions to be improved in k12-432
-  export function parse(val: string): any
-  export function compare(v1: any, v2: any, options?: { simplify: boolean, form: boolean }): any
+  export function parse(val: string): { parsed: boolean, expr: any }
+  export function compare(firstParsedExpr: any, secondParsedExpr: any, options?: { simplify?: boolean, form?: boolean }): { equal: boolean, message: string | null }
 }
